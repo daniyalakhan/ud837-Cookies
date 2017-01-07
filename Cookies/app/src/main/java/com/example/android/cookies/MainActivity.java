32040861da85f9025b14f9df4ccf8d13541de1b3
daniyalakhan <daniyalakhan@programmer.net>
2017-01-08 04:39:18 +0500

    Modified (method: eatCookie) to change the Image Resource in MainActivity.java file, 08Jan16

--- Cookies/app/src/main/java/com/example/android/cookies/MainActivity.java
+++ Cookies/app/src/main/java/com/example/android/cookies/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.cookies;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
      */
     public void eatCookie(View view) {
         // TODO: Find a reference to the ImageView in the layout. Change the image.
-        
+        ImageView androidCookieImageView = (ImageView) findViewById(R.id.android_cookie_image_view);
+        androidCookieImageView.setImageResource(R.drawable.after_cookie);
 
         // TODO: Find a reference to the TextView in the layout. Change the text.
 
