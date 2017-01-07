33cba40a934ef23030793ea7a69e0832aa02653d
daniyalakhan <daniyalakhan@programmer.net>
2017-01-08 04:47:30 +0500

    Modified (method: eatCookie) to set the text of (TextView: status_text_view) in MainActivity.java file, 08Jan16

--- Cookies/app/src/main/java/com/example/android/cookies/MainActivity.java
+++ Cookies/app/src/main/java/com/example/android/cookies/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         androidCookieImageView.setImageResource(R.drawable.after_cookie);
 
         // TODO: Find a reference to the TextView in the layout. Change the text.
-
+        TextView statusTextView = (TextView) findViewById(R.id.status_text_view);
+        statusTextView.setText("I'm so full");
     }
 }
