66264e76995bf2ff008d70433b0eb4567ef3ef28
daniyalakhan <daniyalakhan@programmer.net>
2017-01-08 04:24:35 +0500

    Copied the MainActivity.java code provided in Udacity's lecture 22 (Lesson 3A) to MainActivity.java file of Cookies app, 08Jan16

--- Cookies/app/src/main/java/com/example/android/cookies/MainActivity.java
+++ Cookies/app/src/main/java/com/example/android/cookies/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.cookies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+    public void eatCookie(View view) {
+        // TODO: Find a reference to the ImageView in the layout. Change the image.
+        
+
+        // TODO: Find a reference to the TextView in the layout. Change the text.
+
     }
 }
